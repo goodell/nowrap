0cfe11ee6fd8efe985fc2be6cb075154752d78d7
Dave Goodell <davidjgoodell@gmail.com>
2012-05-01 20:22:33 -0500

    add a cheesy test suite
    It could be a lot fancier, but it should be enough for now.  Fixing
    utf8, tab expansion, and escape sequences in the input will be nearly
    impossible without some sort of regression tests.
    
    tc5 uses a fancy Thai character that doesn't seem to render quite right
    under Vim in my OS X Terminal.app.  I'm not sure if I've botched the
    character somehow or if OS X is just buggy about this...

---
+++ tests/test.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+#
+# a simple test script for a few more exotic wrapping cases
+#
+
+# allow override from the environment
+: ${NOWRAP:=../nowrap}
+: ${DIFF:=diff -q}
+
+#set -x
+
+rm -f *.out BOGUS
+
+do_test() {
+    prefix="$1"
+    args="$2"
+
+$NOWRAP $args $prefix.in > $prefix.out
+if $DIFF $prefix.expected $prefix.out ; then
+    :
+else
+    echo "ERROR: $prefix failed"
+    err=1
+fi
+}
+
+# ==== case 0: simple text, asking for narrower
+do_test tc0 --columns=72
+
+# ==== case 1: simple text but asking for wider wrap than most of the text
+do_test tc1 --columns=140
+
+# ==== case 2: simple text but don't specify columns
+do_test tc2
+
+# ==== case 3: colored text, don't specify columns
+assumed_cols=238
+if test `tput cols` != $assumed_cols ; then
+    echo "columns are "`tput cols`" not $assumed_cols, skipping test 3"
+else
+    do_test tc3
+fi
+
+# ==== case 4: colored text, ask for narrower
+do_test tc4 --columns=72
+
+# ==== case 5: utf-8 text with wide characters
+do_test tc5 --columns=72
+
+# ==== case 6: TAB handling
+do_test tc6 --columns=72
+
+if test -z "$err" ; then
+    echo "PASS"
+    rm -f *.out BOGUS
+else
+    echo "FAIL"
+    exit 1
+fi
+
